fe4847e53c3c04e738fd92b69373ab472ce89b2f
aymen <benmohammed.a.a@gmail.com>
2024-05-04 21:57:23 +0100

    working to fix googlesinin

--- app/src/(auth)/Signin.tsx
+++ app/src/(auth)/Signin.tsx
@@ -10,17 +10,23 @@ import axios from "axios";
 import { StatusBar } from "expo-status-bar";
 
 import {
-  GoogleOneTapSignIn,
-  statusCodes,
   GoogleSignin,
+  statusCodes,
 } from "@react-native-google-signin/google-signin";
 
 browser.maybeCompleteAuthSession();
+
+GoogleSignin.configure({
+  webClientId: process.env.OUSSAMA_Client_ID, // client ID of type WEB for your server. Required to get the `idToken` on the user object, and for offline access.
+  androidClientId: process.env.CLIENT_ID_ANDROID,
+});
 
 export default function Signin() {
   const [token, settoken] = useState<string | undefined>();
 
   const [user, setuser] = useState(null);
+
+  const [userinfo, setuserinfo] = useState();
 
   const [request, response, promptasync] = Google.useAuthRequest({
     clientId: process.env.OUSSAMA_Client_ID,
@@ -28,41 +34,6 @@ export default function Signin() {
   });
 
   const [State, setState] = useState();
-
-  const signIn = async () => {
-    try {
-      await GoogleSignin.hasPlayServices();
-      const userInfo = await GoogleOneTapSignIn.signIn({
-        webClientId: process.env.OUSSAMA_Client_ID,
-        // only needed if you're not using the Firebase config file
-        nonce: "your_nonce",
-      });
-      setState({ userInfo });
-    } catch (error) {
-      if (isErrorWithCode(error)) {
-        switch (error.code) {
-          case statusCodes.NO_SAVED_CREDENTIAL_FOUND:
-            // Android only. No saved credential found, try calling `createAccount`
-            break;
-          case statusCodes.SIGN_IN_CANCELLED:
-            // sign in was cancelled
-            break;
-          case statusCodes.ONE_TAP_START_FAILED:
-            // Android and Web only, you probably have hit rate limiting.
-            // On Android, you can still call `presentExplicitSignIn` in this case.
-            // On the web, user needs to click the `WebGoogleSigninButton` to sign in.
-            break;
-          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-            // Android-only: play services not available or outdated
-            break;
-          default:
-          // something else happened
-        }
-      } else {
-        // an error that's not related to google sign in occurred
-      }
-    }
-  };
 
   // useEffect(() => {
   //   if (response?.type === "success") {
@@ -81,6 +52,34 @@ export default function Signin() {
   //   const user = res.json();
   //   setuser(user);
   // }
+
+  const _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const user = await GoogleSignin.signIn().then((res) => {
+        console.log(res);
+      });
+    } catch (error) {
+      console.log(error);
+      // if (isErrorWithCode(error)) {
+      //   switch (error.code) {
+      //     case statusCodes.SIGN_IN_CANCELLED:
+      //       // user cancelled the login flow
+      //       break;
+      //     case statusCodes.IN_PROGRESS:
+      //       // operation (eg. sign in) already in progress
+      //       break;
+      //     case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+      //       // play services not available or outdated
+      //       break;
+      //     default:
+      //     // some other error happened
+      //   }
+      // } else {
+      //   // an error that's not related to google sign in occurred
+      // }
+    }
+  };
   return (
     <View style={[Gstyles.container, { backgroundColor: "white" }]}>
       <StatusBar backgroundColor={COLORSS.Green}></StatusBar>
@@ -91,7 +90,12 @@ export default function Signin() {
       <View style={Styles.FooterContainer}>
         <View style={Styles.FooterContent}>
           <View style={Styles.SinginButtons}>
-            <TouchableOpacity style={Styles.Button}>
+            <TouchableOpacity
+              style={Styles.Button}
+              onPress={() => {
+                _signIn();
+              }}
+            >
               <Image
                 style={{ width: 60, height: 60, resizeMode: "center" }}
                 source={GoogleI}
